d8483d68a53c795b76eb2bc762edea023e5b2eba
Pablo Colqui <pablo.colqui@gmail.com>
2020-03-18 23:14:37 -0300

    Updates

---
+++ precache-manifest.a64c78c4bb426c66290e7fb8ff6f610b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b5105a4e4e3c7b66a8f4a8f1dcd33b6",
+    "url": "/proyecto-prestamos/index.html"
+  },
+  {
+    "revision": "1efb2d8a43e1180f44ac",
+    "url": "/proyecto-prestamos/static/css/main.7e2e9933.chunk.css"
+  },
+  {
+    "revision": "a28e1d4cd72571ae1d47",
+    "url": "/proyecto-prestamos/static/js/2.dca6f86c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/proyecto-prestamos/static/js/2.dca6f86c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1efb2d8a43e1180f44ac",
+    "url": "/proyecto-prestamos/static/js/main.5ad75f5b.chunk.js"
+  },
+  {
+    "revision": "1d9f11f77ec324e13cd6",
+    "url": "/proyecto-prestamos/static/js/runtime-main.831f085a.js"
+  }
+]);
--- src/components/Mensaje.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Mensaje = () => ( 
-  <p>Agrega una cantidad y plazo a pagar para cotizar</p>
- )
-
- 
-export default Mensaje 
